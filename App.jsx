c552962bbee6e2a26b30a6295b66263645993e51
Tamar Stern <tamarstern@Tamars-MacBook-Pro.local>
2016-12-06 00:00:57 +0200

    initial commit

---
+++ App.jsx
@@ -0,0 +1,98 @@
+import React from 'react';
+
+
+
+class App extends React.Component {
+   
+   constructor() {
+      super();
+		
+      this.state =  {
+             data: []
+         
+      };
+
+      this.updateState = this.updateState.bind(this);
+   }
+    
+
+   updateState(e) {
+      var fromMale = e.fromMale;
+      var fromFemale = e.fromFemale;
+      if(fromMale == true) {
+          this.setState({
+            data:  [
+            {
+               "name":"Oren"
+            },				
+            {
+               "name":"Lior"
+            },
+            {
+               "name":"Alon"
+            }
+         ]
+        });
+      } else if(fromFemale == true) {
+          this.setState({
+            data:  [
+             {
+               "name":"Tamar"
+            },				
+            {
+               "name":"Ayala"
+            },
+            {
+               "name":"Liron"
+            }
+         ]
+        });
+      }
+      
+   }
+   
+   maleButtonClick(e) {
+    e.fromMale = true;
+    e.fromFemale = false;
+    this.updateState(e);
+   }
+
+  femaleButtonClick(e) {
+      e.fromFemale = true;
+      e.fromMale = false;
+      this.updateState(e);
+   }
+
+   
+   render() {
+      
+      return (
+          <div>
+            <h1>Choose Gender</h1>
+            <button type="button" onClick={(e) => this.maleButtonClick(e)}>Male</button>
+            <button type="button" onClick={(e) => this.femaleButtonClick(e)}>Female</button>
+            <table>
+               <tbody>
+                  {this.state.data.map((person, i) => <TableRow key = {i} data = {person} />)}
+               </tbody>
+            </table>
+         </div>
+      );
+   }
+}
+
+
+
+class TableRow extends React.Component {
+   render() {
+      return (
+         <tr>
+            <td>{this.props.data.name}</td>
+         </tr>
+      );
+   }
+}
+
+
+
+export default App;
